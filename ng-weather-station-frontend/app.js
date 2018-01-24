defad1ff93e0d2773084173e591c21ea5971dfec
Alex Ramirez <alexir13@gmail.com>
2018-01-23 23:32:34 -0800

    Remove commented example

--- ng-weather-station-frontend/app.js
+++ ng-weather-station-frontend/app.js
@@ -27,30 +27,3 @@ var server = app.listen(process.env.PORT || 8080, function () {
 });
 
 module.exports = app;
-
-// var express = require('express');
-// var path = require('path');
-// var app = express();
-// app.use(express.static(__dirname));
-
-// app.get('/*', function (req, res) {
-// res.sendFile(path.join(__dirname,'index.html'));
-// });
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-// var err = new Error('Not Found');
-// err.status = 404;
-// next(err);
-// });
-// if (app.get('env') === 'development')
-// {
-// app.listen(3000, function () {
-// console.log('Example app listening on port 3000!');
-// });
-// }
-// else{
-// app.listen(8080, function () {
-// console.log('Example app listening on port 8080!');
-// });
-// }
-// module.exports = app;
